3a5b69dea38b6b90044e7262a93fb3177b1537e4
HaidukYury <HaidukYury@gmail.com>
2017-10-17 21:01:34 +0300

    fixed unused params

--- Source.cpp
+++ Source.cpp
@@ -16,50 +16,25 @@ int main()
 	HDEVINFO hDevInfo;
 	SP_DEVINFO_DATA DeviceInfoData;
 	DWORD i;
-	// Create a HDEVINFO with all present devices.
 	hDevInfo = SetupDiGetClassDevs(NULL,
-		REGSTR_KEY_PCIENUM, // Enumerator
+		REGSTR_KEY_PCIENUM,
 		0,
 		DIGCF_PRESENT | DIGCF_ALLCLASSES);
-
 	if (hDevInfo == INVALID_HANDLE_VALUE)
 	{
-		// Insert error handling here.
 		return 1;
 	}
-	// Enumerate through all devices in Set.
 	DeviceInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
 	for (i = 0; SetupDiEnumDeviceInfo(hDevInfo, i,
 		&DeviceInfoData); i++)
 	{
-		DWORD DataT;
 		LPTSTR buffer = NULL;
 		DWORD buffersize = 0;
-		//
-		// Call function with null to begin with, 
-		// then use the returned buffer size (doubled)
-		// to Alloc the buffer. Keep calling until
-		// success or an unknown failure.
-		//
-		//  Double the returned buffersize to correct
-		//  for underlying legacy CM functions that 
-		//  return an incorrect buffersize value on 
-		//  DBCS/MBCS systems.
-		// 
-		wchar_t *hardwareID;
-		/*// First get requiredLength
-		SetupDiGetDeviceRegistryProperty(deviceInfoList, &deviceInfoData, SPDRP_HARDWAREID, NULL, NULL, 0, &requiredLength);
-
-		hardwareID = (wchar_t*)(new char[requiredLength]());
-
-		// Second call to populate hardwareID
-		SetupDiGetDeviceRegistryProperty(deviceInfoList, &deviceInfoData, SPDRP_HARDWAREID, NULL, (PBYTE)hardwareID, requiredLength, NULL);
-		*/
 		while (!SetupDiGetDeviceRegistryProperty(
 			hDevInfo,
 			&DeviceInfoData,
 			SPDRP_DEVICEDESC|SPDRP_HARDWAREID,
-			&DataT,
+			0,
 			(PBYTE)buffer,
 			buffersize,
 			&buffersize))
@@ -67,19 +42,14 @@ int main()
 			if (GetLastError() ==
 				ERROR_INSUFFICIENT_BUFFER)
 			{
-				// Change the buffer size.
 				if (buffer) LocalFree(buffer);
-				// Double the size to avoid problems on 
-				// W2k MBCS systems per KB 888609. 
 				buffer = (LPTSTR)LocalAlloc(LPTR, buffersize * 2);
 			}
 			else
 			{
-				// Insert error handling here.
 				break;
 			}
 		}
-
 		std::string deviceID;
 		std::string vendorID;
 		std::string Str = buffer;
@@ -87,7 +57,6 @@ int main()
 		std::transform(vendorID.begin(), vendorID.end(), vendorID.begin(), tolower);
 		deviceID = Str.substr(17, 4);
 		std::transform(deviceID.begin(), deviceID.end(), deviceID.begin(), tolower);
-
 		std::ifstream file("pci.ids");
 		std::string line;
 		for (;;)
@@ -123,10 +92,8 @@ int main()
 	if (GetLastError() != NO_ERROR &&
 		GetLastError() != ERROR_NO_MORE_ITEMS)
 	{
-		// Insert error handling here.
 		return 1;
 	}
-	//  Cleanup
 	SetupDiDestroyDeviceInfoList(hDevInfo);
 	system("pause");
 	return 0;
